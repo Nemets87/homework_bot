d4dc37d8eb72cff23c2e7e30998c630908e43a88
Sergey Fedorov <bonustime161@yandex.ru>
2022-10-05 22:46:46 +0300

    My bo54 commit

--- homework.py
+++ homework.py
@@ -15,14 +15,14 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 6 * 100
+RETRY_TIME = 6 * 10 * 10
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_VERDICTS = {
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.',
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
+    'reviewing': 'Работа взята на проверку Алексеем.',
+    'rejected': 'Работа проверена: у Алексея есть замечания.',
+    'approved': 'Работа проверена: Алексею всё понравилось. Ура!',
 }
 
 logger = logging.getLogger(__name__)
